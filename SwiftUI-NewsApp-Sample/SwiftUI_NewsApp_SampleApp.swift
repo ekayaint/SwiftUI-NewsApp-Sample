e75db395c0dd500c601e216100e7b4f887a492b8
ekayaint <ekayaint@gmail.com>
2023-10-22 17:47:01 +0300

    Initial Commit

---
+++ SwiftUI-NewsApp-Sample/SwiftUI_NewsApp_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NewsApp_SampleApp.swift
+//  SwiftUI-NewsApp-Sample
+//
+//  Created by ekayaint on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NewsApp_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
